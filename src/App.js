a39185a0628c954a3d72237de7adb32d9dbab7eb
Joao Balico <joaobalico12@hotmail.com>
2022-03-02 18:28:36 +0000

    Iteration 5 done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import foodsJSON from './foods.json';
+import React, { useState } from 'react';
+import FoodBox from './Components/Foodbox/Foodbox';
+import AddFoodForm from './Components/AddFoodForm/AddFoodForm';
+import Searchbar from './Components/Searchbar/Searchbar';
 
 function App() {
+  const [foods, setFoods] = useState(foodsJSON);
+
+  const [displayFoods, setDisplayFoods] = useState(foodsJSON)
+
+  const addNewFood = (newFood) => {
+    const updatedFoods = [...foods, newFood];
+    setFoods(updatedFoods);
+  };
+
+  const searchFilter = (searchQuery) => {
+    let filteredFoods = foods.filter((food) =>
+      food.name.toLowerCase().startsWith(searchQuery.toLowerCase())
+    );
+    console.log(filteredFoods);
+    setDisplayFoods(filteredFoods)
+  };
+
+  const deleteFood = (foodID) => {
+    const filteredFoods = foods.filter((food) => {
+      return food !== foodID;
+    });
+    setFoods(filteredFoods);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AddFoodForm addFood={addNewFood}/>
+      <Searchbar search={searchFilter}/>
+      <h1>Food List</h1>
+      {displayFoods.map((food, index) => {
+        return (
+          
+          <FoodBox clickToDelete= {deleteFood} key={index}
+            food={{
+              name: food.name,
+              calories: food.calories,
+              image: food.image,
+              servings: food.servings,
+            }}
+          />
+        );
+      })}
     </div>
   );
 }
