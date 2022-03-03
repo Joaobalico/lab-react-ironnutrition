1a685c1d86d0509455c4b3f84baae66e88e8e4ff
Joao Balico <joaobalico12@hotmail.com>
2022-03-03 08:30:29 +0000

    Added delete function

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import React, { useState } from 'react';
 import FoodBox from './Components/Foodbox/Foodbox';
 import AddFoodForm from './Components/AddFoodForm/AddFoodForm';
 import Searchbar from './Components/Searchbar/Searchbar';
+import { Row, Divider, Button } from 'antd';
 
 function App() {
   const [foods, setFoods] = useState(foodsJSON);
 
-  const [displayFoods, setDisplayFoods] = useState(foodsJSON)
+  const [displayFoods, setDisplayFoods] = useState(foodsJSON);
+
+  const [showForm, setForm] = useState(true)
 
   const addNewFood = (newFood) => {
     const updatedFoods = [...foods, newFood];
@@ -20,27 +23,35 @@ function App() {
       food.name.toLowerCase().startsWith(searchQuery.toLowerCase())
     );
     console.log(filteredFoods);
-    setDisplayFoods(filteredFoods)
+    setDisplayFoods(filteredFoods);
   };
 
   const deleteFood = (foodName) => {
-    console.log('clicking')
+    console.log('clicking');
     const deletedFoods = foods.filter((food) => {
       return food.name !== foodName;
     });
     setDisplayFoods(deletedFoods);
-    setFoods(deletedFoods)
+    setFoods(deletedFoods);
+  };
+
+  const toggleShow = () => {
+    setForm(!showForm);
   };
 
   return (
     <div className="App">
-      <AddFoodForm addFood={addNewFood}/>
-      <Searchbar search={searchFilter}/>
+      <AddFoodForm addFood={addNewFood} />
+      <Searchbar search={searchFilter} />
+      <Button onClick={toggleShow}>
+        {showForm ? 'Hide' : 'Add New Food'}{' '}
+      </Button>
       <h1>Food List</h1>
       {displayFoods.map((food, index) => {
         return (
-          
-          <FoodBox clickToDelete= {deleteFood} key={index}
+          <FoodBox
+            clickToDelete={deleteFood}
+            key={index}
             food={{
               name: food.name,
               calories: food.calories,
